fa2073f44ae488e1fca774ac67bb572002f2a1b6
Andrew Hack <andrew.hack242@gmail.com>
2020-11-29 18:06:59 -0500

    player now has a steve head

--- SimpleObject.cpp
+++ SimpleObject.cpp
@@ -2,8 +2,8 @@
 
 SimpleObject::SimpleObject(Vector3D pos,  Vector3D scale, Vector3D rotation) :
  Object(pos, scale, rotation){
-     std::vector<std::string> texture = {"Images/BinImage.bmp"};
-     std::vector<bool> hasAlpha = {false};
+     std::vector<std::string> texture = {"Images/steve_front.bmp", "Images/steve_back.bmp", "Images/steve_right.bmp", "Images/steve_left.bmp", "Images/steve_top.bmp", "Images/steve_bottom.bmp", };
+     std::vector<bool> hasAlpha = {false, false, false, false, false, false};
         LoadTextures(texture, hasAlpha);
 }
 SimpleObject::~SimpleObject(){
@@ -19,12 +19,13 @@ void SimpleObject::DrawShape(){
     //glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,black);
 
     glEnable(GL_TEXTURE_2D);
-    glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE, GL_MODULATE);    
+    glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE, GL_MODULATE);
     glColor3f(1,1,1);
-    glBindTexture(GL_TEXTURE_2D, textures[0]);
 
     //  Front
-    glColor3f(1,0,0);
+    //glColor3f(1,0,0);
+    glBindTexture(GL_TEXTURE_2D, textures[2]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//unblur the texture  
     glBegin(GL_QUADS);
     glNormal3f( 0, 0, 1);
     glTexCoord2f(0,0); glVertex3f(-1,-1, 1);
@@ -34,7 +35,9 @@ void SimpleObject::DrawShape(){
     glEnd();
 
     //  Back
-    glColor3f(0,0,1);
+    //glColor3f(0,0,1);
+    glBindTexture(GL_TEXTURE_2D, textures[3]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);    
     glBegin(GL_QUADS);
     glNormal3f( 0, 0,-1);
     glTexCoord2f(0,0); glVertex3f(+1,-1,-1);
@@ -44,7 +47,9 @@ void SimpleObject::DrawShape(){
     glEnd();
 
     //  Right
-    glColor3f(1,1,0);
+    //glColor3f(1,1,0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);    
     glBegin(GL_QUADS);
     glNormal3f(+1, 0, 0);
     glTexCoord2f(0,0); glVertex3f(+1,-1,+1);
@@ -54,7 +59,9 @@ void SimpleObject::DrawShape(){
     glEnd();
 
     //  Left
-    glColor3f(0,1,0);
+    //glColor3f(0,1,0);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);    
     glBegin(GL_QUADS);
     glNormal3f(-1, 0, 0);
     glTexCoord2f(0,0); glVertex3f(-1,-1,-1);
@@ -64,7 +71,9 @@ void SimpleObject::DrawShape(){
     glEnd();
 
     //  Top
-    glColor3f(0,1,1);
+    //glColor3f(0,1,1);
+    glBindTexture(GL_TEXTURE_2D, textures[4]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);    
     glBegin(GL_QUADS);
     glNormal3f( 0,+1, 0);
     glTexCoord2f(0,0); glVertex3f(-1,+1,+1);
@@ -74,7 +83,9 @@ void SimpleObject::DrawShape(){
     glEnd();
     
     //  Bottom
-    glColor3f(1,0,1);
+    //glColor3f(1,0,1);
+    glBindTexture(GL_TEXTURE_2D, textures[5]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);    
     glBegin(GL_QUADS);
     glNormal3f( 0,-1, 0);
     glTexCoord2f(0,0); glVertex3f(-1,-1,-1);
